777fb6ad1ccacbaf059293ee6331cc51f0cc2691
ninahovinmaa <nina.hovinmaa@enfogroup.com>
2022-01-05 11:42:13 +0100

    add validation to commentform for dishes

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -20,6 +20,23 @@ export class DishdetailComponent implements OnInit {
   next: string;
   commentForm: FormGroup;
   comment: Comment;
+  @ViewChild('cform') commentFormDirective: any;
+
+  formErrors: any = {
+    rating: '',
+    comment: '',
+    author: ''
+  }
+
+  validationMessages: any = {
+    'comment': {
+      'required': 'Comment is required'
+    },
+    'author': {
+      'required': 'Name is required',
+      'minlength': 'Name must be at least 2 characters long'
+    }
+  }
 
   constructor(private dishService: DishService, 
     private route: ActivatedRoute,
@@ -55,7 +72,43 @@ export class DishdetailComponent implements OnInit {
   createForm() {
     this.commentForm = this.fb.group({
       rating: 5,
-      comment:'',
+      comment:['', Validators.required],
+      author: ['', [Validators.required, Validators.minLength(2)]],
+    })
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data))
+  }
+
+  onValueChanged(data?: any){
+    if (!this.commentForm) {
+      return
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        //clear previous error message (if any)
+        this.formErrors[field]='';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    //set form back to pristine
+    this.commentFormDirective.resetForm();
+    //reset values to default
+    this.commentForm.reset({
+      rating: 5,
+      comment: '',
       author: '',
     })
   }
